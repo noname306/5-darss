553f81d00ce6236b70aa774cd3e04e0512f020f7
noname306 <ya.sitora11@gmail.com>
2025-01-08 14:34:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const nam = prompt("Enter your name");
+const log = +prompt ("Enter your login")
+const pass = +prompt ("Enter your password")
+
+const Mars = {
+    ism: nam,
+    login: log,
+    password: pass,
+}
+
+console.log(Mars);
